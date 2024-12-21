919599d39cb8140bb1b5ae5202fd4f02986624e2
publicprofile123 <145284020+kukuffsh@users.noreply.github.com>
2024-12-21 06:59:33 +0300

    Update login_router.py

--- src/routers/login_router.py
+++ src/routers/login_router.py
@@ -18,16 +18,3 @@ async def login(uid: int):
     raise HTTPException(status_code=404, detail="Item Not Found")
 
 
-@router.post("/register")
-async def create_user(user: user_dto.UserCreate, db: AsyncSession):
-    async with db.begin():
-        existing_user = await db.execute(select(models.UserRow).filter_by(email=user.email))
-        if existing_user.scalars().first():
-            raise HTTPException(status_code=400, detail="Email already registered")
-
-        new_user = models.UserRow(email=user.email, role=user.role)
-        db.add(new_user)
-
-    await db.commit()
-
-    return {"id": new_user.id, "email": new_user.email, "role": new_user.role}
